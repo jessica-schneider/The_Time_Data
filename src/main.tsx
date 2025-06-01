aed7a812be306dcdb15cc5dc52216f6aea573d33
Jessica Schneider <jessicaannschneider@gmail.com>
2025-06-01 16:16:58 -0600

    CHORE : linting

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,9 @@ import { ConvexProvider, ConvexReactClient } from "convex/react";
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ConvexProvider client={convex}>
-      <App />
-    </ConvexProvider>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<ConvexProvider client={convex}>
+			<App />
+		</ConvexProvider>
+	</React.StrictMode>,
 );
